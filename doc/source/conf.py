ecad68c12f58cd72b702e0c300938ad70167d824
Marcus Schäfer <marcus.schaefer@gmail.com>
2024-10-10 19:28:04 +0200

    Fix packaging for Leap
    Use the right python interpreter for Leap

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -90,7 +90,7 @@ html_sidebars = {
 
 html_theme = "sphinx_rtd_theme"
 
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 html_theme_options = {
     'collapse_navigation': False,
